f2a16bc01bb14a055096476b49dbd4feb05973bd
Henrik <henriks@gmail.com>
2022-08-02 16:50:47 +0200

    grid with curves

--- tutorial-2-3-curves-grid.js
+++ tutorial-2-3-curves-grid.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ]
@@ -10,10 +11,64 @@ const sketch = ({width, height}) => {
   const numCells = cols * rows;
   const gw = width * 0.8;
   const gh = height * 0.8;
+  const cw = gw / cols;
+  const ch = gh / rows;
+  const mx = (width - gw) * 0.5;
+  const my = (height - gh) * 0.5;
+
+  let x, y, n;
+  let frequency = 0.002;
+  let amplitude = 90;
+  const points = [];
+  for(let i = 0; i < numCells; i++){
+    x = (i % cols) * cw;
+    y = Math.floor(i / cols) * ch;
+
+    n = random.noise2D(x, y, frequency, amplitude);
+    x += n;
+    y += n;
+
+    points.push(new Point({x, y}));
+  }
 
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
+
+    context.save();
+    context.translate(mx, my);
+    context.translate(cw * 0.5, ch * 0.5);
+    context.strokeStyle = 'red';
+    context.lineWidth = 4;
+
+    // draw lines
+    for(let r = 0; r < rows; r++){
+      context.beginPath();
+      for(let c = 0; c < cols - 1; c++){
+        const i = r* cols + c;
+        const curr = points[i];
+        const next = points[i + 1];
+        
+        const mx = curr.x + (next.x - curr.x) * 0.5;
+        const my = curr.y + (next.y - curr.y) * 0.5;
+
+        if(c == 0){
+          context.moveTo(curr.x, curr.y)
+        } else if (i == cols.length - 2) {
+          context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
+        } else {
+          context.quadraticCurveTo(curr.x, curr.y, mx, my);
+        }
+      }
+      context.stroke();
+    }
+
+    // draw points
+    // points.forEach(point => {
+    //   point.draw(context);
+    // });
+
+    context.restore();
   };
 };
 
