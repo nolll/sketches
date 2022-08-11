33631024561c6cbbca2d37926009da5fcfc0b6dd
Henrik <henriks@gmail.com>
2022-08-11 18:44:54 +0200

    started animating wind

--- tree.js
+++ tree.js
@@ -1,8 +1,12 @@
 const canvasSketch = require('canvas-sketch');
 const { random } = require('canvas-sketch-util');
 
+const seed = random.getRandomSeed();
+
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  name: `tree-${seed}`,
+  animate: true
 };
 
 const greens = [
@@ -14,6 +18,7 @@ const greens = [
 ];
 
 const sketch = ({width, height}) => {
+  random.setSeed(seed);
   const tree = new Tree(width, height);
   
   return ({ context, width, height }) => {
@@ -21,6 +26,7 @@ const sketch = ({width, height}) => {
     context.fillRect(0, 0, width, height);
     context.lineCap = 'round';
 
+    tree.update();
     tree.draw(context);
   };
 };
@@ -33,31 +39,39 @@ class Tree{
     const y = canvasHeight;
     const angle = -Math.PI * 0.5;
     const level = 1;
-    this.trunk = new Branch(length, lineWidth, x, y, angle, level);
+    this.trunk = new Branch(length, lineWidth, x, y, angle, level, null);
   }
 
   draw(context){
     this.trunk.draw(context);
   }
+
+  update(){
+    this.trunk.update();
+  }
 }
 
 class Branch{
-  constructor(length, lineWidth, x, y, angle, level){
+  constructor(length, lineWidth, x, y, angle, level, parent){
     this.length = random.range(length * 0.5, length * 0.75);
     this.lineWidth = lineWidth;
+    this.parent = parent;
     this.x = x;
     this.y = y;
     this.angle = angle;
     this.tx = this.x + Math.cos(this.angle) * this.length;
     this.ty = this.y + Math.sin(this.angle) * this.length
     this.level = level;
+    this.color = this.level > 4
+      ? random.pick(greens)
+      : 'black';
     this.branches = [];
 
     if(length > 5){
       const numBranches = random.range(3, 5);
       for(let i = 0; i < numBranches; i++){
         const a = random.range(this.angle - Math.PI * 0.35, this.angle + Math.PI * 0.35);
-        this.branches.push(new Branch(length * 0.5, lineWidth * 0.5, this.tx, this.ty, a, this.level + 1));
+        this.branches.push(new Branch(length * 0.5, lineWidth * 0.5, this.tx, this.ty, a, this.level + 1, this));
       }
     }
   }
@@ -65,15 +79,22 @@ class Branch{
   draw(context){
     context.lineWidth = this.lineWidth;
   
-    context.strokeStyle = this.level > 4
-      ? random.pick(greens)
-      : 'black';
+    context.strokeStyle = this.color;
     context.beginPath();
-    context.moveTo(this.x, this.y);
+    const x = this.parent ? this.parent.tx : this.x;
+    const y = this.parent ? this.parent.ty : this.y;
+    context.moveTo(x, y);
     context.lineTo(this.tx, this.ty);
     context.stroke();
   
     this.branches.forEach(branch => branch.draw(context));
+  }
+
+  update(){
+    this.tx++;
+    // this.branches.forEach(branch => {
+    //   branch.update();
+    // });
   }
 }
 
