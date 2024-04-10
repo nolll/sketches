40c9e5920f5ea8a04b3e3b0f52fa5a0e0ddc08f2
Henrik <henriks@gmail.com>
2024-04-10 19:30:48 +0200

    A little more random and a little more round

--- projects/flowers.js
+++ projects/flowers.js
@@ -5,7 +5,7 @@ const colormap = require("colormap");
 
 const settings = {
   dimensions: [1080, 1080],
-  animate: true,
+  animate: false,
 };
 
 const sketch = ({ context, width, height }) => {
@@ -13,7 +13,8 @@ const sketch = ({ context, width, height }) => {
   let angles = [];
 
   for (var i = 0; i < leafCount; i++) {
-    angles.push(((2 * Math.PI) / leafCount) * i);
+    var rnd = random.range(-0.1, 0.1);
+    angles.push(((2 * Math.PI) / leafCount) * i + rnd);
   }
 
   angles = random.shuffle(angles);
@@ -28,26 +29,44 @@ const sketch = ({ context, width, height }) => {
     for (var i = 0; i < leafCount; i++) {
       context.save();
       context.translate(width / 2, height / 2);
-      context.scale(0.8, 0.8);
+      var rnd = random.range(-0.1, 0.1);
+      context.scale(0.8 + rnd, 0.8 + rnd);
       var angle = angles[i];
       context.rotate(angle);
 
       const topPoint = { x: 0, y: 0 };
-      const rightPoint = { x: 200, y: 300 };
+      const rightPoint = { x: 100, y: 300 };
       const bottomPoint = { x: 0, y: 600 };
-      const leftPoint = { x: -200, y: 300 };
+      const leftPoint = { x: -100, y: 300 };
+
+      const topRightControlPoint = { x: 100, y: 0 };
+      const bottomRightControlPoint = { x: 100, y: 600 };
+      const bottomLeftControlPoint = { x: -100, y: 600 };
+      const topLeftControlPoint = { x: -100, y: 0 };
 
       context.beginPath();
       context.moveTo(topPoint.x, topPoint.y);
       context.quadraticCurveTo(
+        topRightControlPoint.x,
+        topRightControlPoint.y,
         rightPoint.x,
-        rightPoint.y,
+        rightPoint.y
+      );
+      context.quadraticCurveTo(
+        bottomRightControlPoint.x,
+        bottomRightControlPoint.y,
         bottomPoint.x,
         bottomPoint.y
       );
       context.quadraticCurveTo(
+        bottomLeftControlPoint.x,
+        bottomLeftControlPoint.y,
         leftPoint.x,
-        leftPoint.y,
+        leftPoint.y
+      );
+      context.quadraticCurveTo(
+        topLeftControlPoint.x,
+        topLeftControlPoint.y,
         topPoint.x,
         topPoint.y
       );
@@ -61,17 +80,25 @@ const sketch = ({ context, width, height }) => {
         rightPoint.y
       );
 
-      gradient.addColorStop(0.1, "green");
-      gradient.addColorStop(0.25, "pink");
-      gradient.addColorStop(0.5, "red");
-      gradient.addColorStop(0.75, "pink");
-      gradient.addColorStop(0.9, "green");
+      gradient.addColorStop(0.1, "orange");
+      gradient.addColorStop(0.25, "yellow");
+      gradient.addColorStop(0.5, "orange");
+      gradient.addColorStop(0.75, "yellow");
+      gradient.addColorStop(0.9, "orange");
 
       context.fillStyle = gradient;
       context.fill();
 
       context.restore();
     }
+
+    // context.save();
+    // context.translate(width / 2, height / 2);
+    // context.fillStyle = "red";
+    // context.beginPath();
+    // context.arc(0, 0, 100, 0, 2 * Math.PI); // Control point two
+    // context.fill();
+    // context.restore();
   };
 };
 
